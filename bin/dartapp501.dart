4d64cbd70badbfe5368c941cd1ce50435cf9a826
patipan-patisampita <patipan.patisampita@gmail.com>
2021-11-29 10:48:56 +0700

    initial commit

---
+++ bin/dartapp501.dart
@@ -0,0 +1,12 @@
+void main(){
+  var province = 'Chantaburi';
+  province = 'trat';
+  
+  final int? a;
+  int b = 10;
+
+  print(province.toUpperCase());
+  
+  
+  print(b);
+}
---
+++ bin/dartapp502.dart
@@ -0,0 +1,10 @@
+void main(){
+  final String name ; //Statically type
+  const int age  = 37; //Statically type
+  var id =12345;//Dynamically type
+  name = 'Steav';
+  
+  print(name);
+  print(age);
+  print(id);
+}
